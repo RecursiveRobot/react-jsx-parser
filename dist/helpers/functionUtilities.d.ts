c2bbfe7cdec2dd5ab63d414d3d21e5ebaab2533b
Attie Naude <attie.naude@live.com>
2025-07-30 13:52:54 +0200

    v1.40.0

---
+++ dist/helpers/functionUtilities.d.ts
@@ -0,0 +1,6 @@
+import * as AcornJSX from 'acorn-jsx';
+export declare function isSpreadElement(node: AcornJSX.BaseExpression): node is AcornJSX.SpreadElement;
+export declare function getClosureBindings(fullExpression: AcornJSX.Expression): string[];
+export declare function getRenderFunction(jsx: string, bindings: Record<string, any>, parseExpression: (exp: AcornJSX.Expression, scope?: Record<string, any>) => any): (args: Record<string, any>) => any;
+export declare function getAllJsxElements(code: string): (AcornJSX.JSXElement | AcornJSX.JSXFragment)[];
+export declare function transpileFunctionBody(body: string, bindings: Record<string, any>, parseExpression: (exp: AcornJSX.Expression, scope?: Record<string, any>) => any): [string, Record<string, any>];
